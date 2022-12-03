bff6a43f74ad703bc7c7942ba051db8b66e09f52
Ravin <87692753+RavinClaw@users.noreply.github.com>
2022-12-03 23:03:01 +0000

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,6 @@
+import os
+
+def Initiate():
+  os.system("Starting Process")
+  os.system("Unable to locate rest of boot.py")
+  os.system("This is a temp. file as of current for testing purposes")
